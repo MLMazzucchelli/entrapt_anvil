566b37c1fdef1c2190ce027ad767dd13b91420f7
mlmazzucc@gmail.com <mlmazzucc@gmail.com>
2024-10-18 21:42:57 +0000

    Edited settings, Initial_page and Calculate_entrapment

--- client_code/Initial_page/__init__.py
+++ client_code/Initial_page/__init__.py
@@ -87,6 +87,10 @@ class Initial_page(Initial_pageTemplate):
       self.content_panel.clear()
       self.content_panel.add_component(Project(), index=0)
 
+    def sidebar_menu_clicked(self, clicked_item, **event_args):
+      if clicked_item == ""
+      pass
+
 
 
 
